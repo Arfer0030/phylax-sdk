7839f8722a9542dca829d057ce52a1df4a89e463
w1th0ut <bagas.gunardi67@gmail.com>
2026-05-26 13:12:33 +0700

    chore: stop tracking next build artifacts

--- web/.next/server/app/api/[...path]/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/[...path]/route.js")
-R.c("server/chunks/[externals]_next_dist_11bl3x3._.js")
-R.c("server/chunks/[root-of-the-server]__0d~i6n0._.js")
-R.c("server/chunks/web__next-internal_server_app_api_[___path]_route_actions_0zd4qtf.js")
-R.m(62483)
-module.exports=R.m(62483).exports
